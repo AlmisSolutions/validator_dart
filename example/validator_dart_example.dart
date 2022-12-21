e163ff22c72a9e39092f1c5d6110ef007a4017f5
Almis90 <almisb90@gmail.com>
2022-12-21 12:40:04 +0200

    Initial commit

---
+++ example/validator_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:validator_dart/validator_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/validator_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library validator_dart;
+
+export 'src/validator_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
